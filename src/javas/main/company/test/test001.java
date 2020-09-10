c8fc19c6c535fc2b20fc093434757f89629a4157
wjk <1527008697@qq.com>
2020-09-10 17:59:42 +0800

    测试

---
+++ src/javas/main/company/test/test001.java
@@ -0,0 +1,17 @@
+package javas.main.company.test;
+
+import java.util.*;
+
+/**
+ * @description:
+ * @author: wjk
+ * @date: 2020/9/10 15:30
+ **/
+public class test001 {
+    public static void main(String[] args) {
+        List<String> l = new ArrayList<>();
+        List<String> l1 = new LinkedList<>();
+        Map<String,String> map = new HashMap<>();
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    }
+}
