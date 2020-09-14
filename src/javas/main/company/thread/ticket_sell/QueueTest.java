0a4e511cfc13e242303c804cce4f5a35be9b4b5e
wjk <1527008697@qq.com>
2020-09-14 17:58:19 +0800

    测试

---
+++ src/javas/main/company/thread/ticket_sell/QueueTest.java
@@ -0,0 +1,30 @@
+package javas.main.company.thread.ticket_sell;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+/**
+ * @description:
+ * @author: wjk
+ * @date: 2020/9/14 17:06
+ **/
+public class QueueTest {
+    public static Queue<String> queue = new ConcurrentLinkedDeque<>();
+    static {
+        for (int i = 0; i < 1000; i++) {
+            queue.add("i:"+i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                while(true) {
+                    if (queue.isEmpty())
+                        break;
+                    System.out.println(queue.poll());
+                }
+            }).start();
+        }
+    }
+}
